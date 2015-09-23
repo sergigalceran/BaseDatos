6c512312b6e7091a6d955994a7aabf44e637f13f
sergigalceran <sergi_galce7@hotmail.com>
2015-09-23 16:54:41 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package edu.upc.dsa;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+            MySQLAccess dao = new MySQLAccess();
+            dao.readDataBase();
+    }
+}
